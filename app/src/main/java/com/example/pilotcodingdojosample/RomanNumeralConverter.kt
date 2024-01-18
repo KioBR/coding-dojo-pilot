b61fb9f4abad668760f93db9bb0da8c828e9d28a
Caio Fernandes <furacaio@gmail.com>
2024-01-18 15:56:13 -0300

    feat: Dojo Sample

---
+++ app/src/main/java/com/example/pilotcodingdojosample/RomanNumeralConverter.kt
@@ -0,0 +1,20 @@
+package com.example.pilotcodingdojosample
+
+class RomanNumeralConverter {
+
+  fun toInt(romanNumeral: String): Int {
+    return 0
+  }
+
+  // Só poderá ser usado um algarismo romano caso como conversor:
+
+  /* PROIBIDO: when(romanNumeral) {
+    'II' -> 2
+    'IV' -> 4
+   */
+
+  /* PERMITIDO: when(romanNumeral) {
+     'I' -> 1
+     'V' -> 5
+  */
+}
---
+++ app/src/test/java/com/example/pilotcodingdojosample/RomanNumeralTest.kt
@@ -0,0 +1,13 @@
+package com.example.pilotcodingdojosample
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class RomanNumeralTest {
+
+  @Test
+  fun testConverterI() {
+    val romanNumeralConverter = RomanNumeralConverter()
+    //assertEquals(expected, result)
+  }
+}
